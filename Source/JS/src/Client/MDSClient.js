1b2fe8438ad9aa29277d31dae99ca3b923e7756c
Stevo <stevo@sunsetmagicwerks.com>
2022-08-04 15:43:57 -0700

    Updated MDSClient to require different version of p-queue to be compatible with CommonJS deployments. Updated MDSClient to work with node deployments.
    Released as 2.0.27.

--- Source/JS/src/Client/MDSClient.js
+++ Source/JS/src/Client/MDSClient.js
@@ -6,7 +6,7 @@
 //
 
 // Imports
-// let fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+let fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 let	PQueue = require('p-queue').default;
 // import PQueue from 'p-queue';
 
